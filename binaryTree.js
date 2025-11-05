f1da03001a19eaf2b9cf8de5ef7d43c2b38b7e29
chandan vishwakarma <chandanvishw11002@gmail.com>
2025-11-05 19:17:17 +0530

    preorder and postorder is done

--- binaryTree.js
+++ binaryTree.js
@@ -15,6 +15,24 @@ function inOrderTraversal(node){
     console.log(node.data);
     inOrderTraversal(node.right);
 }
+
+function preOrderTraversal(node){
+    if(node == null){
+        return;
+    }
+    console.log(node.data);
+    preOrderTraversal(node.left);
+    preOrderTraversal(node.right);
+}
+
+function postOrderTraversal(node){
+    if(node == null){
+        return;
+    }
+    postOrderTraversal(node.left);
+    postOrderTraversal(node.right);
+    console.log(node.data);
+}
 const root = new TreeNode(1);
 root.left = new TreeNode(2);
 root.right = new TreeNode(3);
@@ -24,4 +42,6 @@ root.left.right = new TreeNode(5);
 root.right.left = new TreeNode(6);
 root.right.right = new TreeNode(7);
 
-inOrderTraversal(root);
+// inOrderTraversal(root);/
+// preOrderTraversal(root)
+postOrderTraversal(root)
